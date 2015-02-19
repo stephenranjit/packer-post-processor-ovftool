62ab96c75385a21c3d502d64885fbebfa431c9bb
root <root@packer1.eng.btc.netapp.in>
2015-02-19 16:42:29 +0530

    fixing get location

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/iancmcc/packer-post-processor-ovftool/ovftool"
+	"github.com/stephenranjit/packer-post-processor-ovftool/ovftool"
 	"github.com/mitchellh/packer/packer/plugin"
 )
 
